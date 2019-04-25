7763317001d0b48ed53018de3ae30abf64c0f6e9
m0tela01 <michael.telahun1@gmail.com>
2019-04-25 19:49:20 -0400

    Add logic for database update from file. Reads SensorRead.txt and updates Firebase continuosly.

--- sensorToDataBase.py
+++ sensorToDataBase.py
@@ -1,43 +1,68 @@
 import os
 import json
+import pyrebase
+import requests
 
-source = "C:\\Users\\Michael Telahun\\Documents\\OBD2Database\\SensorRead.txt"
+source = "C:\\Users\\MichaelTelahun\\Documents\\CECS 525\\FinalProject\\OBD2Database\\SensorRead.txt"
+headers = ["RPM", "Cool Temp", "Throttle Position", "Engine Load"]
 
+# data sample
 # 760.5 revolutions_per_minute
 # 90 degC
 # 3.9215686274509802 percent
 # 28.627450980392158 percent
 # **************
+# data sample
 
+# firebase config and authentication
+config = {
+    'apiKey': 'AIzaSyCJV79IhlSdhtAOBLJz9H5dAGxfzjSau4Y',
+    'authDomain': 'webobdbase.firebaseapp.com',
+    'databaseURL': 'https://webobdbase.firebaseio.com',
+    'projectId': 'webobdbase',
+    'storageBucket': 'webobdbase.appspot.com',
+    'messagingSenderId': '184771792235'
+}
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+email = 'mockemailman@gmail.com'
+password = 'JASVT1#PD?3@'
+user = auth.sign_in_with_email_and_password(email, password)
+db = firebase.database()
+# firebase config and authentication
 
-data = ["RPM", "Cool Temp", "Throttle Position", "Engine Load"]
-
-senorData = open("SensorRead.txt")
+#   CHANGE THIS TO THE LIVE DATA
+sensorData = open("SensorRead.txt")
+#load object
 obdData = open('obdData.json', 'w')
 
 head = True
 index = 0
-
-for item in senorData:
-    if senorData == "**************":
-        obdData.write(']')
-        break
-        
-
 obdData.write('[\n')
-for row in reader:
-    if head:
-        head = False
-        continue
+
+for item in sensorData:
+    # write to firebase prepare for next object
+    if index == len(headers):
+        obdData.write(']')
+        obdData.close()
+        with open ("obdData.json",'r') as dataToWrite:
+            data=json.load(dataToWrite)
+        db.child("").remove()
+        results = db.child('').set(data, user['idToken'])
+        print('\n( ͡° ͜ʖ ͡°)')
+        obdData = open('obdData.json', 'w')
+        obdData.write('[\n')
+        index = 0
+    # last row in set
+    elif index == len(headers) - 1:
+        row = [headers[index - 1] + ":" + item.rstrip()]
+        json.dump(row,obdData)
+        index = index + 1
+        obdData.write('\n')
+    # all other rows in set
     else:
-        if index < count:
-            json.dump(row,obdData)
-            obdData.write(',\n')
-            index = index + 1
-        elif index == count:
-            json.dump(row,obdData)
-            obdData.write('\n')
-            index = index + 1
-        else:
-            break
-obdData.write(']')
+        row = [headers[index] + ":" + item.rstrip()]
+        json.dump(row,obdData)
+        obdData.write(',\n')
+        index = index + 1
+        
